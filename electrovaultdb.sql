84a80e9913e785aad81f4f365d1ed6b98e558cf7
Mujipro1 <mshafqat.bscs21seecs@seecs.edu.pk>
2025-06-06 23:29:36 +0500

    db added

--- electrovaultdb.sql
+++ electrovaultdb.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `ads`
 --
-
+create db if not exists online_marketplace;
+use online_marketplace;
 DROP TABLE IF EXISTS `ads`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
